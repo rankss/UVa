8616df5f0516572c1d412db0287d4b32c2655b9c
RankSS <yangbeny@sfu.ca>
2019-09-06 01:27:09 -0700

    UVa Problem 11172

---
+++ 11172.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	uint64_t a, b;
+	int t;
+
+	cin >> t;
+	while (t--)
+	{
+		cin >> a >> b;
+		if (a > b) cout << ">" << endl;
+		else if (b > a) cout << "<" << endl;
+		else cout << "=" << endl;
+	}
+	return 0;
+}
